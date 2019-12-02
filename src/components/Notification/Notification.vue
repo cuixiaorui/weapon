88ae562097f07e692e7d5a8a320db176ae86b406
cuixiaorui <cui_xiaorui@126.com>
2019-12-02 19:52:30 +0800

    feat(notification): add notification

---
+++ src/components/Notification/Notification.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wp-notification">
+    <div class="wp-notification__title">
+      {{ title }}
+    </div>
+
+    <div class="wp-notification__message">
+      {{ message }}
+    </div>
+
+    <button
+      v-if="showClose"
+      class="wp-notification__close-button"
+      @click="onCloseHandler"
+    ></button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    message: {
+      type: String,
+      default: ''
+    },
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+    onClose: {
+      type: Function,
+      default: () => {}
+    }
+  },
+  methods: {
+    onCloseHandler() {
+        this.onClose();
+    }
+  }
+};
+</script>
---
+++ src/components/Notification/__tests__/Notification.spec.js
@@ -0,0 +1,59 @@
+import Notification from "../Notification.vue";
+import { shallowMount } from "@vue/test-utils";
+
+describe("Notification", () => {
+  it("应该有类名为 wp-notification 得 div", () => {
+    const wrapper = shallowMount(Notification);
+    const result = wrapper.contains(".wp-notification");
+    expect(result).toBe(true);
+  });
+
+  describe("props", () => {
+    it("title - 可以通过 title 设置标题", () => {
+      const wrapper = shallowMount(Notification, {
+        propsData: {
+          title: "test"
+        }
+      });
+
+      const titleContainer = wrapper.find(".wp-notification__title");
+      expect(titleContainer.text()).toBe("test");
+    });
+
+    it("message - 可以通过 message 设置说明文字", () => {
+      const message = "这是一段说明文字";
+      const wrapper = shallowMount(Notification, {
+        propsData: {
+          message
+        }
+      });
+
+      const container = wrapper.find(".wp-notification__message");
+      expect(container.text()).toBe(message);
+    });
+
+    it("showClose - 控制显示按钮", () => {
+      // 默认显示按钮
+      const wrapper = shallowMount(Notification);
+      const btnSelector = ".wp-notification__close-button";
+      expect(wrapper.contains(btnSelector)).toBe(true);
+      wrapper.setProps({
+        showClose: false
+      });
+      expect(wrapper.contains(btnSelector)).toBe(false);
+    });
+
+    it("点击关闭按钮后，应该调用传入的 onClose ", () => {
+      const onClose = jest.fn();
+      const btnSelector = ".wp-notification__close-button";
+      const wrapper = shallowMount(Notification, {
+        propsData: {
+          onClose
+        }
+      });
+
+      wrapper.find(btnSelector).trigger("click");
+      expect(onClose).toBeCalledTimes(1);
+    });
+  });
+});
